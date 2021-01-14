d85a67c908883036a4b681ce3b595f66b4037a0e
ericrevans <eric.evans@gsa.gov>
2021-01-14 08:19:21 -0500

    Add Lambda/Serverless resources

---
+++ serverless/bin/serverless.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { ServerlessStack } from '../lib/serverless-stack';
+
+const app = new cdk.App();
+new ServerlessStack(app, 'ServerlessStack');
